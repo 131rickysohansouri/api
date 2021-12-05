2302b8a1bf9dec8eb2397045b88a1976abeef3d5
131rickysohansouri <68801841+131rickysohansouri@users.noreply.github.com>
2021-12-05 21:04:15 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,106 @@
+# Importing essential libraries and modules
+from flask import Flask, render_template, request, Markup,redirect
+from flask.templating import _render
+import numpy as np
+import pandas as pd
+from utils.disease import disease_dic
+import pickle
+import io
+import torch
+from torchvision import transforms
+from PIL import Image
+from utils.model import ResNet9
+from flask import jsonify
+
+app=Flask(__name__)
+
+disease_classes = ['Apple___Apple_scab',
+                   'Apple___Black_rot',
+                   'Apple___Cedar_apple_rust',
+                   'Apple___healthy',
+                   'Blueberry___healthy',
+                   'Cherry_(including_sour)___Powdery_mildew',
+                   'Cherry_(including_sour)___healthy',
+                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                   'Corn_(maize)__Common_rust',
+                   'Corn_(maize)___Northern_Leaf_Blight',
+                   'Corn_(maize)___healthy',
+                   'Grape___Black_rot',
+                   'Grape__Esca(Black_Measles)',
+                   'Grape__Leaf_blight(Isariopsis_Leaf_Spot)',
+                   'Grape___healthy',
+                   'Orange__Haunglongbing(Citrus_greening)',
+                   'Peach___Bacterial_spot',
+                   'Peach___healthy',
+                   'Pepper,bell__Bacterial_spot',
+                   'Pepper,bell__healthy',
+                   'Potato___Early_blight',
+                   'Potato___Late_blight',
+                   'Potato___healthy',
+                   'Raspberry___healthy',
+                   'Soybean___healthy',
+                   'Squash___Powdery_mildew',
+                   'Strawberry___Leaf_scorch',
+                   'Strawberry___healthy',
+                   'Tomato___Bacterial_spot',
+                   'Tomato___Early_blight',
+                   'Tomato___Late_blight',
+                   'Tomato___Leaf_Mold',
+                   'Tomato___Septoria_leaf_spot',
+                   'Tomato___Spider_mites Two-spotted_spider_mite',
+                   'Tomato___Target_Spot',
+                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                   'Tomato___Tomato_mosaic_virus',
+                   'Tomato___healthy']
+
+# Training 
+disease_model_path = 'models/plant_disease_model.pth'
+disease_model = ResNet9(3, len(disease_classes))
+disease_model.load_state_dict(torch.load(
+    disease_model_path, map_location=torch.device('cpu')))
+disease_model.eval()
+
+# Prediction
+def predict_image(img, model=disease_model):
+    """
+    Transforms image to tensor and predicts disease label
+    :params: image
+    :return: prediction (string)
+    """
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+    ])
+    image = Image.open(io.BytesIO(img))
+    img_t = transform(image)
+    img_u = torch.unsqueeze(img_t, 0)
+
+    # Get predictions from model
+    yb = model(img_u)
+    # Pick index with highest probability
+    _, preds = torch.max(yb, dim=1)
+    prediction = disease_classes[preds[0].item()]
+    # Retrieve the class label
+    return prediction
+
+
+@app.route('/',methods=['POST','GET'])
+# predicting plant health (image is received from POST request)
+def disease_prediction():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return redirect(request.url)
+        try:
+            img = file.read()
+            prediction = predict_image(img)
+            prediction = str(disease_dic[prediction])
+            return render_template("result.html",pred=prediction)
+        except:
+            pass
+    return render_template("blank.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
